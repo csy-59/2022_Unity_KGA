966166c10c2de38a8c9e1bf6e13d87eef6936d0d
csy-59 <csy010509@naver.com>
2022-07-07 15:42:29 +0900

    220707_(오후1) 스트립트를 통한 이벤트 주기 학습

--- Metaverse1/Assets/LogTest.cs
+++ Metaverse1/Assets/LogTest.cs
@@ -1,24 +1,49 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class LogTest : MonoBehaviour
 {
+    private void Awake()
+    {
+        Debug.Log("여기는 Awake");
+    }
+    private void OnEnable()
+    {
+        Debug.Log("OnEable()");
+    }
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log("Start()");
+        // 에러 로그
+        //Debug.LogError("태스트를 위한 에러 보고");
+
+        Destroy(gameObject, 3f); 
+        // gameObject == 해당 컴포넌트를 갖고 있는 게임 오브젝트를 의미(Cube)
+        // 3f => 3초 뒤에 자기자신을 파괴
+    }
+    private void FixedUpdate()
+    {
+        Debug.Log("FixedUpdate()");
     }
 
     // Update is called once per frame
     void Update()
     {
-        // 일반 로그
-        Debug.Log("이것은 일반 로그입니다.");
+        Debug.Log("Update()");
+    }
 
-        // 경고 로그
-        Debug.LogWarning("이것은 경고 로그입니다.");
+    private void LateUpdate()
+    {
+        Debug.Log("LateUpdate()");
+    }
 
-        // 에러 로그
-        Debug.LogError("이것은 에러 로그입니다.");
+    private void OnDisable()
+    {
+        Debug.Log("OnDisable()");
+    }
+
+    private void OnDestroy()
+    {
+        Debug.Log("OnDestroy()");
     }
 }
