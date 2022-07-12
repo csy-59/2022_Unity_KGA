e555162782a94e5a1cbd7cf94e6e5604788b51a1
csy-59 <csy010509@naver.com>
2022-07-12 12:50:46 +0900

    220712(오전)_충돌처리 실습(수업)

---
+++ Dodge(220708)/Assets/Script/Pro_Bullet/CollisionTest.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionTest : MonoBehaviour
+{
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    Debug.Log($"[OnCollisionEnter] Me: {gameObject.name}, Other: {collision.gameObject.name}");
+    //}
+    //
+    //private void OnCollisionStay(Collision collision)
+    //{
+    //    Debug.Log($"[OnCollisionStay] Me: {gameObject.name}, Other: {collision.gameObject.name}");
+    //}
+    //
+    //private void OnCollisionExit(Collision collision)
+    //{
+    //    Debug.Log($"[OnCollisionExit] Me: {gameObject.name}, Other: {collision.gameObject.name}");
+    //}
+    //
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    Debug.Log($"[OnTriggerEnter] Me: {gameObject.name}, Other: {other.name}");
+    //}
+    //
+    //private void OnTriggerStay(Collider other)
+    //{
+    //    Debug.Log($"[OnTriggerStay] Me: {gameObject.name}, Other: {other.name}");
+    //}
+    //
+    //private void OnTriggerExit(Collider other)
+    //{
+    //    Debug.Log($"[OnTriggerExit] Me: {gameObject.name}, Other: {other.name}");
+    //}
+}
