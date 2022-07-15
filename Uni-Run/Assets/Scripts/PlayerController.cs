2f7cc9a2e6b5cdcc84f3333618fae439201b3706
csy-59 <csy010509@naver.com>
2022-07-15 17:39:56 +0900

    220715(오후2)_애니메이션 효과 적용

--- Uni-Run/Assets/Scripts/PlayerController.cs
+++ Uni-Run/Assets/Scripts/PlayerController.cs
@@ -2,19 +2,22 @@
 
 // PlayerController는 플레이어 캐릭터로서 Player 게임 오브젝트를 제어한다.
 public class PlayerController : MonoBehaviour {
-   public AudioClip deathClip; // 사망시 재생할 오디오 클립
-   public float jumpForce = 700f; // 점프 힘
+   public AudioClip DeathClip; 
+   public float JumpForce = 700f;
 
-   private int jumpCount = 0; // 누적 점프 횟수
-   private bool isGrounded = false; // 바닥에 닿았는지 나타냄
-   private bool isDead = false; // 사망 상태
+   private int jumpCount = 0; 
+   private bool isGrounded = false; 
+   private bool isDead = false; 
 
-   private Rigidbody2D playerRigidbody; // 사용할 리지드바디 컴포넌트
-   private Animator animator; // 사용할 애니메이터 컴포넌트
-   private AudioSource playerAudio; // 사용할 오디오 소스 컴포넌트
+   private Rigidbody2D playerRigidbody; 
+   private Animator animator; 
+   private AudioSource playerAudio; 
 
    private void Start() {
-       // 초기화
+        // 초기화
+        playerRigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
    }
 
    private void Update() {
